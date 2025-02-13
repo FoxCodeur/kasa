15c5defefc5cb15ee2a90c7d972aa49a57c53fc2
Daisy <detcherry@gmail.com>
2025-02-13 13:06:35 +0100

    Fix routing issue with HashRouter for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // Composant qui permet la gestion du routage
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./sass/index.scss";
 import App from "./components/App/App.jsx";
 
-// Créer la racine et rendre l'application avec StrictMode et BrowserRouter
+// Créer la racine et rendre l'application avec StrictMode et HashRouter
 createRoot(document.getElementById("root")).render(
-  //On encapsule l'application dans StrictMode et BrowserRouter
+  //On encapsule l'application dans StrictMode et HashRouter
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
